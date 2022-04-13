4b8c0823947965832edb8f79f8cab50ebfc9dd30
Ivan Haliurov <galyurov@gmail.com>
2022-04-13 23:54:42 +0300

    json example

--- exampleJSON.js
+++ exampleJSON.js
@@ -1,3 +1,25 @@
-/**
- * ПРИМЕР ИСПОЛЬЗОВАНИЯ JSON
- */
+// формат json обьекта который приходит с сервера
+
+{
+  "userId": 1,
+  "id": 1,
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit"
+}
+
+const someData = {
+  name: 'Test',
+  age: 22,
+  phone: '000-000-0'
+}
+
+
+// привод объект к json строке
+JSON.stringify(someData) // '{"name":"Test","age":22,"phone":"000-000-0"}'
+
+JSON.parse('{"name":"Test","age":22,"phone":"000-000-0"}')
+// {
+//   name: 'Test', 
+//   age: 22, 
+//   phone: '000-000-0'
+// }

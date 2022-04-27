3364f416ded8f27c478f32c4e464946c957dbcef
Vidadi920 <vidadi886@gmail.com>
2022-04-27 22:46:38 +0300

    so so

--- simpleExamples/exampleFetch.js
+++ simpleExamples/exampleFetch.js
@@ -4,40 +4,39 @@
  *  Вместо цепочки then используйте async\await
  */
 
-
 const URL = 'https://jsonplaceholder.typicode.com/posts';
 
-function getWithFetch() {
-    fetch(URL)
-        .then((response) => response.json())
-        .then((json) => {
-            /**
-            * как вы можете переписать promise chaining
-             * через async await?
-            */
-            console.log(json); // получили массив из 100 постов
-            // сделали запрос чтобы получить подробные данные поста №45
-            fetch(`${URL}/${json[45].id}`)
-                .then((response) => response.json())
-                .then((data) => console.log(data))
-        });
+async function getWithFetch() {
+  let response = await fetch(URL);
+  let arr = await response.json();
+  console.log(arr);
+  /**
+   * как вы можете переписать promise chaining
+   * через async await?
+   */
+  console.log(json); // получили массив из 100 постов
+  // сделали запрос чтобы получить подробные данные поста №45
+  let respon = await fetch(`${URL}/${json[45].id}`);
+  let data = await respon.json();
+  console.log(data);
 }
 
-function postWithFetch() { // сохранение нового поста
-    fetch(URL, {
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'test post 53x',
-            body: 'lorem',
-            userId: 1,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+async function postWithFetch() {
+  // сохранение нового поста
+  let response = await fetch(URL, {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'test post 53x',
+      body: 'lorem',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  let resp = await response.json();
+  console.log(resp);
 }
 
-//getWithFetch();
-//postWithFetch();
+getWithFetch();
+postWithFetch();
--- simpleExamples/exampleXML.js
+++ simpleExamples/exampleXML.js
@@ -5,35 +5,45 @@
  *  Выводите в консоль текст код-статуса ответа
  */
 
-
 const postsURL = 'https://jsonplaceholder.typicode.com/posts';
 
 function getWithXhr() {
-    // создание инстанса объекта
-    let xhr = new XMLHttpRequest();
-    // инициализация соединения с параметрами
-    xhr.open('GET', postsURL)
-    xhr.send() // отправка запроса
-    xhr.onload = ()=> { // обработчик при загрузке
-        console.log(JSON.parse(xhr.response));
-    }
-    xhr.onerror = ()=> { // обработчик при ошибке
-    }
+  // создание инстанса объекта
+  let xhr = new XMLHttpRequest();
+  // инициализация соединения с параметрами
+  xhr.open('GET', postsURL);
+  xhr.send(); // отправка запроса
+  xhr.onload = () => {
+    // обработчик при загрузке
+
+    console.log(JSON.parse(xhr.response));
+    console.log(`Успех номер статуса:${xhr.status} текст: ${xhr.statusText}`);
+  };
+  xhr.onerror = () => {
+    console.log(`Ошибка номер статуса:${xhr.status} текст: ${xhr.statusText}`);
+  };
 }
 
 function postWithXhr() {
-    let xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
 
-    xhr.open('POST', postsURL)
-    xhr.send(JSON.stringify({ // тело запроса(данные)
-        title: 'test post 53x',
-        body: 'lorem',
-        userId: 1,
-    }))
-    xhr.onload = ()=> {
-        console.log(JSON.parse(xhr.response));
-    }
+  xhr.open('POST', postsURL);
+  xhr.send(
+    JSON.stringify({
+      // тело запроса(данные)
+      title: 'test post 53x',
+      body: 'lorem',
+      userId: 1,
+    })
+  );
+  xhr.onload = () => {
+    console.log(JSON.parse(xhr.response));
+    console.log(`Успех номер статуса:${xhr.status} текст: ${xhr.statusText}`);
+  };
+  xhr.onerror = () => {
+    console.log(`Ошибка номер статуса:${xhr.status} текст: ${xhr.statusText}`);
+  };
 }
 
-//getWithXhr();
-//postWithXhr();
+getWithXhr();
+postWithXhr();
